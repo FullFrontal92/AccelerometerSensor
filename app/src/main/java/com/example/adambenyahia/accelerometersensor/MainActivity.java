ec74b84498f2b781355f1b5495133db77f230be3
FullFrontal92 <adam.benyahia@hotmail.fr>
2018-09-20 15:53:37 +0200

    vibrate

--- app/src/main/java/com/example/adambenyahia/accelerometersensor/MainActivity.java
+++ app/src/main/java/com/example/adambenyahia/accelerometersensor/MainActivity.java
@@ -5,8 +5,12 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
@@ -33,8 +37,31 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager.unregisterListener(this);
     }
 
+    float xOld=0;
+    float yOld=0;
+    float zOld=0;
+    float ThreaShold=3000;
+    long OldTime=0;
+
     @Override
     public void onSensorChanged(SensorEvent event) {
+        float x=event.values[0];
+        float y=event.values[1];
+        float z=event.values[2];
+        long CurrentTime= System.currentTimeMillis();
+
+        if((CurrentTime-OldTime)>100){
+
+            long TimeDiff=CurrentTime-OldTime;
+            OldTime=CurrentTime;
+
+            float speed=Math.abs(x+y+z-xOld-yOld-zOld)/TimeDiff *10000;
+            if (speed>ThreaShold){
+                Vibrator v=(Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(3000);
+                Toast.makeText(getApplicationContext(),"Shock",Toast.LENGTH_LONG).show();
+            }
+        }
 
     }
 
